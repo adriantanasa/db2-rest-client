bdc7df00a1bbbc12ed232cf0e9c04fbbdaded434
Adrian Tanasa <adrian_tanasa@yahoo.com>
2018-09-12 14:21:46 +0100

    [TASK 001] Update the integration tests to not depend on a specific schema

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # db2-on-cloud-rest
 
-Node.js client for DB2 on Cloud Warehouse REST Apis (previously dashDB).
+Node.js client for IBM Db2 Warehouse on Cloud REST API (previously dashDB).
+It is intended to be used for DevOps (administration, monitoring, data load) for DB2 on Cloud service.
+
+The target APIs are covering the following main areas:
+
+* Authentication
+* Database Objects
+* Data load
+* SQL
+* File Storage
+* Monitoring
+* Settings
+* Users
 
 ## Installation
+
+Installing the client locally and using it in a Node.JS application:
 
 ```
 npm i db2-on-cloud-rest --save
@@ -37,18 +51,33 @@ try {
 
 TBD
 
+Check the /test/integration folder for examples of usage.
+
 ## Jobs
 
 TBD
 
 ## Integration Testing
 
-Runing test agains real environment:
+Running integration tests against your DB2 on Cloud instance - the user needs access to create tables, execute queries, load data. The credentials can be found on the **IBM on Cloud > DB2 Service > Credentials** page. 
 
 ```
-export DB_USERID='bluadmin';export DB_PASSWORD='<password>';export DB_URI='https://<hostname>/dbapi/v3';export DEBUG=db2-on-cloud-rest; npm run integration
+export DB_USERID='<userid>';export DB_PASSWORD='<password>';export DB_URI='https://<hostname>/dbapi/v3'; npm run integration
+```
+
+## Debugging
+
+```
+# all log levels
+export DEBUG=db2-on-cloud-rest
+# just info
+export DEBUG=db2-on-cloud-rest:info
+# all
+export DEBUG=db2-on-cloud-rest:*
 ```
 
 ## References
 
-https://dashdb-txnha-flex-yp-dal09-104.services.dal.bluemix.net/console/api/index.html
+* [IBM Db2 Warehouse on Cloud REST API](https://developer.ibm.com/static/site-id/85/api/db2whc-v3/)
+* [IBM DB2 on Cloud](https://www.ibm.com/cloud/db2-on-cloud)
+* [ibm_db module](https://www.npmjs.com/package/ibm_db)
--- test/integration/index.js
+++ test/integration/index.js
@@ -1,16 +1,17 @@
-const debug = require('debug')('db2-on-cloud-rest');
-const DB2RestClient = require('../../lib/DB2RestClient');
-
+const debugNotice = require('debug')('db2-on-cloud-rest:notice');
+const DB2RestClient = require('../../lib/Db2RestClient');
 const expect = require('chai').expect;
 let client;
 
-// TBD mock endpoint - create sample data
-describe('Testsuite INTEGRATION', () => {
+/**
+ * Creates a new table and attempts to perform a series of operations
+ */
+describe('INTEGRATION tests', () => {
 
-    before(function() {
+    before(async () => {
         // runs before all tests in this block
         const {DB_PASSWORD: password, DB_USERID: userId, DB_URI: uri} = process.env;
-        debug('Credentials <userid=%s, pass=%s, uri=%s>', userId, password, uri);
+        debugNotice('Credentials <userid=%s, pass=%s, uri=%s>', userId, password, uri);
         const config = {
             credentials: {
                 userid: userId,
@@ -19,20 +20,32 @@ describe('Testsuite INTEGRATION', () => {
             uri: uri
         };
         client = new DB2RestClient(config);
+        try {
+            await client.bulkQueries('DROP TABLE INTEGR.TST_SAMPLE');
+        } catch (err) {
+            debugNotice('Expected to fail if table doesn\'t exist.', err.message);
+        }
+        await client.bulkQueries(`CREATE TABLE INTEGR.TST_SAMPLE (ID CHAR(5) NOT NULL,
+        DESCRIPTION VARCHAR(200) NOT NULL, PRIMARY KEY(ID))`);
     });
 
-    it('Test request success', async () => {
-        let data = await client.request(undefined, {uri: '/schemas'});
+    after(async () => {
+        await client.bulkQueries('DROP TABLE INTEGR.TST_SAMPLE');
+    });
+
+
+    it('Test request method', async () => {
+        let data = await client.request(undefined, {uri: '/schemas/INTEGR/tables'});
         expect(data).to.be.ok;
-        expect(data['count']).to.equal(33);
+        expect(data.count).to.equal(1);
     });
 
-    it('Test requestPolling failure', async () => {
+    it('Test requestPolling method with failure', async () => {
         let error;
         try {
             let data = await client.request('SQL_JOBS', {
                 body: {
-                    'commands': 'SELECT COUNT(*) FROM COMPANY.COMPANY;SELECT COUNT(*) FROM COMPANY.COMPANY2;'
+                    'commands': 'SELECT COUNT(*) FROM INTEGR.TST_SAMPLE;SELECT COUNT(*) FROM INTEGR.NOT_THERE;'
                 }
             });
             await client.requestPolling(undefined, {uri: '/sql_jobs/' + data['id']});
@@ -44,43 +57,51 @@ describe('Testsuite INTEGRATION', () => {
         expect(error.statusCode).to.equal(500);
     });
 
-    it('Test requestPolling success', async () => {
+    it('Test requestPolling method with success', async () => {
         let data = await client.request('SQL_JOBS',
             {
                 body: {
-                    'commands': 'SELECT COUNT(*) FROM COMPANY.COMPANY WHERE 1;'
+                    'commands': `SELECT COUNT(*) FROM INTEGR.TST_SAMPLE;
+                    INSERT INTO INTEGR.TST_SAMPLE VALUES ('0010', 'Some data')`
                 }
             });
         data = await client.requestPolling(null, {uri: '/sql_jobs/' + data['id']});
         expect(data).to.deep.include({status: 'completed'});
     });
 
-    it('Test query success', async () => {
-        let data = await client.query('SELECT CODE FROM MANUAL.PG_TO_LEVEL20GBT ORDER BY CODE LIMIT 0,2');
+    it('Test query method with success', async () => {
+        let data = await client.query('SELECT ID, DESCRIPTION FROM INTEGR.TST_SAMPLE WHERE 1');
         expect(data).to.be.ok;
-        expect(data.length).to.equal(2);
+        expect(data.length).to.equal(1);
+        expect(data[0]).to.deep.equal({ID: '0010', DESCRIPTION: 'Some data'});
     });
 
-    it('Test upload success', async () => {
-        let data = await client.upload('./test/data/test.csv');
+    it('Test upload method with success', async () => {
+        let data = await client.upload('./test/data/sample2.csv');
         expect(data).to.be.ok;
         expect(data.resources[0]).to.deep.include({
-            'name': 'test.csv',
-            'location': '/home/load/test.csv'
+            'name': 'sample2.csv',
+            'location': '/home/load/sample2.csv'
         });
     });
 
-    it('Test loadJob success', async () => {
-        await client.upload('./test/data/test.csv');
-        let data = await client.loadFromServer('test.csv', 'LOAD_TEST', 'MANUAL', true);
+    it('Test loadJob method with success', async () => {
+        await client.bulkQueries('DELETE FROM INTEGR.TST_SAMPLE WHERE 1');
+        await client.upload('./test/data/sample2.csv');
+        let data = await client.loadFromServer('sample2.csv', 'TST_SAMPLE', 'INTEGR', false);
         expect(data).to.be.ok;
         expect(data.status.status).to.equal('Success');
+        let loadRes = await client.query('SELECT COUNT(*) AS TOTAL FROM INTEGR.TST_SAMPLE');
+        expect(loadRes[0]['TOTAL']).to.equal('2');
     });
 
-    it('Test loadJob success', async () => {
-        let data = await client.load('./test/data/test.csv', 'LOAD_TEST', 'MANUAL', true);
+    it('Test load method with success', async () => {
+        // load / upload is by default REPLACE
+        let data = await client.load('./test/data/sample1.csv', 'TST_SAMPLE', 'INTEGR');
         expect(data).to.be.ok;
         expect(data.status.status).to.equal('Success');
+        let loadRes = await client.query('SELECT COUNT(*) AS TOTAL FROM INTEGR.TST_SAMPLE');
+        expect(loadRes[0]['TOTAL']).to.equal('4');
     });
 
 });
